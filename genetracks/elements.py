a87aca4ec0a6b2450c3b9671f88027afe82f09cc
jeff-k <jeff@x-0.ca>
2018-11-01 15:01:59 -0700

    elements inherit from figure

--- genetracks/elements.py
+++ genetracks/elements.py
@@ -1,11 +1,13 @@
 import xml.etree.ElementTree as ET
 
-class diagram:
-    def __init__(self):
-        self.polys = [
-            [(0, 500), (10, 510), (0, 10), (500, 510)],]
+class Figure:
+    def __init__(self, items=[]):
+        self.items = items
 
-    def draw(self):
+    def to_svg(self, g):
+        pass
+
+    def show(self):
         svg = ET.Element('svg', xmlns="http://www.w3.org/2000/svg",
                 version="1.1")
         g = ET.SubElement(svg,"g",style="fill-opacity:1.0; stroke:black;")
@@ -13,14 +15,19 @@ class diagram:
             item.to_svg(g)
         return ET.tostring(svg)
 
-class track:
-    def __init__(self, style='clothesline'):
+class Box(Figure):
+    def __init__(self):
+        self.polys = [
+            [(0, 500), (10, 510), (0, 10), (500, 510)],]
+
+class Track(Figure):
+    def __init__(self, label=None, style='clothesline'):
         pass
 
-class label:
+class Label(Figure):
     def __init__(self, text, style=None):
         pass
 
-class alignment:
+class Alignment(Figure):
     def __init__(self, text, style=None):
         pass
