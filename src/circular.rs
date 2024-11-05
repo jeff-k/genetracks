c79e0525de4f9d0344871a58bcf840989f68de2e
jeff-k <jeff_k@fastmail.com>
2024-11-05 04:02:43 +0000

    add text labels and tick marks

--- src/circular.rs
+++ src/circular.rs
@@ -135,17 +135,16 @@ pub fn Bar(
         <>
             <path d=path stroke=color.to_string() stroke_width="2" fill="none" />
             {label
-                .map(move |text| {
-                    let (x, y) = text_pos();
+                .map(|text| {
                     view! {
                         <text
-                            x=x
-                            y=y
+                            x=move || text_pos().0
+                            y=move || text_pos().1
                             text-anchor="middle"
                             dominant-baseline="middle"
                             fill="black"
                             font-size="smaller"
-                            font-family="sans"
+                            font-family="monospace"
                         >
                             {text}
                         </text>
@@ -156,14 +155,90 @@ pub fn Bar(
 }
 
 #[component]
+pub fn Tick(#[prop(into)] pos: f64, #[prop(optional)] label: Option<String>) -> impl IntoView {
+    let parent = use_context::<Memo<TrackCx>>().expect("must have track as parent");
+
+    let text_pos = create_memo(move |_| {
+        let track = parent();
+
+        let angle = (pos / track.length) * 2.0 * PI - PI / 2.0;
+        let text_radius = track.radius + track.track_height + 30.0;
+
+        let x = track.center_x + text_radius * angle.cos();
+        let y = track.center_y + text_radius * angle.sin();
+
+        (x, y)
+    });
+
+    let path = create_memo(move |_| {
+        let track = parent();
+
+        let angle = (pos / track.length) * 2.0 * PI - PI / 2.0;
+
+        let inner_radius = track.radius - track.track_height / 2.0;
+        let outer_radius = track.radius + track.track_height + 20.0;
+
+        let (inner_x, inner_y) = (
+            track.center_x + inner_radius * angle.cos(),
+            track.center_y + inner_radius * angle.sin(),
+        );
+
+        let (outer_x, outer_y) = (
+            track.center_x + outer_radius * angle.cos(),
+            track.center_y + outer_radius * angle.sin(),
+        );
+
+        format!("M {inner_x},{inner_y} L {outer_x},{outer_y}")
+    });
+
+    view! {
+        <g style="z-index: 10000">
+            <path d=path stroke="black" stroke-width="1" fill="none" />
+
+            {label
+                .map(|text| {
+                    view! {
+                        <text
+                            x=move || text_pos().0
+                            y=move || text_pos().1
+                            text-anchor="middle"
+                            dominant-baseline="middle"
+                            fill="black"
+                            font-size="smaller"
+                            font-family="monospace"
+                        >
+                            {text}
+                        </text>
+                    }
+                })}
+
+        </g>
+    }
+}
+
+#[component]
 pub fn Region(
     #[prop(into)] start: f64,
     #[prop(into)] end: f64,
     #[prop(optional)] style: ElemStyle,
+    #[prop(optional)] label: Option<String>,
     #[prop(optional)] color: Colour,
 ) -> impl IntoView {
     //    let cx = use_context::<ReadSignal<FigCx>>().expect("Region must be descendent of Figure");
     let parent = use_context::<Memo<TrackCx>>().expect("Region must be child of Track");
+
+    let text_pos = create_memo(move |_| {
+        let fig = parent();
+
+        let start_angle = (start / fig.length) * 2.0 * PI - PI / 2.0;
+        let end_angle = (end / fig.length) * 2.0 * PI - PI / 2.0;
+        let mid_angle = (start_angle + end_angle) / 2.0;
+
+        let x = fig.center_x + fig.radius * mid_angle.cos();
+        let y = fig.center_y + fig.radius * mid_angle.sin();
+
+        (x, y)
+    });
 
     let path = create_memo(move |_| {
         let fig = parent();
@@ -229,5 +304,25 @@ pub fn Region(
         }
     });
 
-    view! { <path d=path fill=color.to_string() /> }
+    view! {
+        <>
+            <path d=path fill=color.to_string() />
+            {label
+                .map(|text| {
+                    view! {
+                        <text
+                            x=move || text_pos().0
+                            y=move || text_pos().1
+                            text-anchor="middle"
+                            dominant-baseline="middle"
+                            fill="black"
+                            font-size="smaller"
+                            font-family="monospace"
+                        >
+                            {text}
+                        </text>
+                    }
+                })}
+        </>
+    }
 }
